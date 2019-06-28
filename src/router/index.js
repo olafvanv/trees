bdefed799b1c06b9a121fa4ef0cb7f2693b45305
--r <gabriel.moawad@capgemini.com>
2019-06-28 14:56:25 +0200

    stat rough draft

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Donation from '@/components/Donation';
+import Stats from '@/components/Stats.vue'
 
 Vue.use(Router);
 
 const routes = [
-  {path: '/donation', name:'donation', component: Donation}
+  {path: '/donation', name:'donation', component: Donation},
+  {path: '/stats', name:'stats', component: Stats}
 ]
 
 export default new Router({
