7b880301c244dc83da68525ca4ed524bcd3b273c
Georgalas <konstantinos.georgalas@capgemini.com>
2019-06-28 12:20:14 +0200

    first jibberish

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <Park msg="Welcome to Your Vue.js App"/> -->
+    <ParkList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Park from './components/Park.vue';
+import ParkList from './components/ParkList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Park,
+    ParkList
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ParkList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul id="example-1">
+      <li v-for="park in parks">{{park}}</li>
+    </ul>
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  el: "#example-1",
+  data: {
+    parks: ["1", "2", "1"]
+  }
+});
+</script>
