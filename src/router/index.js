b47f4c53e1e2e5a2b1b9702072e72823b2d61983
Olaf van Venetie <olafvv@Olafs-MacBook-Pro.local>
2019-06-28 12:20:42 +0200

    hello world removed

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld';
 import Donation from '@/components/Donation';
 
 Vue.use(Router);
 
 const routes = [
-  {path: '/home', name: 'Home', component: HelloWorld},
   {path: '/donation', name:'donation', component: Donation}
 ]
 
